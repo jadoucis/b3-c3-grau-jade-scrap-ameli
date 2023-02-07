e326e44d06397ebeddce4b0dd12d7a1f26cb319c
jadoucis <jade.grau@epsi.fr>
2023-02-07 15:02:09 +0100

    create soup

--- test.py
+++ test.py
@@ -1,4 +1,7 @@
 import requests
+from bs4 import BeautifulSoup
+
 url = "http://annuairesante.ameli.fr/"
 page = requests.get(url)
-print(page.content)
+soup = BeautifulSoup(page.content, 'html.parser')
+print(soup)
