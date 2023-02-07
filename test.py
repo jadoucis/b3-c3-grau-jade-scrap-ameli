587762cde30900fa479c67ea5b8e642aa232df31
jadoucis <jade.grau@epsi.fr>
2023-02-07 15:50:05 +0100

    use header

--- test.py
+++ test.py
@@ -2,6 +2,16 @@ import requests
 from bs4 import BeautifulSoup
 
 url = "http://annuairesante.ameli.fr/"
-page = requests.get(url)
-soup = BeautifulSoup(page.content, 'html.parser')
+header = {
+    "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 OPR/93.0.0.0"
+}
+
+req = requests.Session()
+page = requests.get(url, headers=header)
+
+if page.status_code == 200:
+    search_url = page.url
+
+soup = BeautifulSoup(page.text, 'html.parser')
+
 print(soup)
