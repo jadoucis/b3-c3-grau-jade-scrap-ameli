fe0d5e37aca93211a0d41eaa92b6a84b33e5dbe3
jadoucis <jade.grau@epsi.fr>
2023-02-07 16:00:13 +0100

    use payload

--- test.py
+++ test.py
@@ -1,13 +1,21 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = "http://annuairesante.ameli.fr/"
+payload = {
+    "type": "ps",
+    "ps_profession": "34",
+    "ps_profession_label": "Médecin généraliste",
+    "ps_localisation": "HERAULT (34)",
+    "localisation_category": "departements",
+}
+
+url = "http://annuairesante.ameli.fr/recherche.html"
 header = {
-    "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 OPR/93.0.0.0"
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 OPR/93.0.0.0"
 }
 
 req = requests.Session()
-page = requests.get(url, headers=header)
+page = requests.post(url, params=payload, headers=header)
 
 if page.status_code == 200:
     search_url = page.url
